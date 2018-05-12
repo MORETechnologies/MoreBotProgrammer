38aad0558b3facece3b8edb2b2d0c6f312947f9a
sazanality <hieu@moretech.co>
2018-05-12 17:37:39 -0500

    Add sleep block cell view

---
+++ MoreBotProgrammer.iOS/Blocks/SleepBlock/SleepBlockViewCell.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Foundation;
+using MoreBotProgrammer.Core;
+using UIKit;
+
+namespace MoreBotProgrammer.iOS
+{
+    public partial class SleepBlockViewCell : BlockViewCell
+    {
+        public static readonly NSString Key = new NSString("SleepBlockViewCell");
+        public static readonly UINib Nib;
+
+        SleepBlockViewModel viewModel;
+
+        static SleepBlockViewCell()
+        {
+            Nib = UINib.FromName("SleepBlockViewCell", NSBundle.MainBundle);
+        }
+
+        protected SleepBlockViewCell(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override BlockViewModel BlockViewModel => viewModel;
+
+        public override void AwakeFromNib()
+        {
+            base.AwakeFromNib();
+
+            OnAwake(cellContainer);
+        }
+
+        public override void SetViewModel(BlockViewModel blockViewModel)
+        {
+            viewModel = (SleepBlockViewModel)blockViewModel;
+
+            sleepTextField.Text = viewModel.Milliseconds;
+        }
+    }
+}
---
+++ MoreBotProgrammer.iOS/Blocks/SleepBlock/SleepBlockViewCell.designer.cs
@@ -0,0 +1,46 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace MoreBotProgrammer.iOS
+{
+    [Register ("SleepBlockViewCell")]
+    partial class SleepBlockViewCell
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView cellContainer { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel sleepLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField sleepTextField { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (cellContainer != null) {
+                cellContainer.Dispose ();
+                cellContainer = null;
+            }
+
+            if (sleepLabel != null) {
+                sleepLabel.Dispose ();
+                sleepLabel = null;
+            }
+
+            if (sleepTextField != null) {
+                sleepTextField.Dispose ();
+                sleepTextField = null;
+            }
+        }
+    }
+}
