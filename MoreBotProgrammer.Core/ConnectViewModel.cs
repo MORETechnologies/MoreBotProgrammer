4489014ab50b613233463cc290e4f16b5ffcddd8
sazanality <hieu@moretech.co>
2018-06-11 18:03:24 -0500

    Add view model for connect screen

---
+++ MoreBotProgrammer.Core/ConnectViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MoreBotProgrammer.Core
+{
+    public class ConnectViewModel
+    {
+        const string DefaultHost = "192.168.4.1";
+        const int DefaultPort = 333;
+
+        AppMain appMain;
+        IBotClient client;
+
+        public ConnectViewModel(AppMain appMain, IBotClient client)
+        {
+            this.appMain = appMain;
+            this.client = client;
+
+            Host = DefaultHost;
+            Port = DefaultPort.ToString();
+        }
+
+        public event EventHandler<string> StatusChanged;
+
+        public string Host { get; private set; }
+
+        public string Port { get; private set; }
+
+        public async Task<bool> Connect(string host, string port)
+        {
+            StatusChanged?.Invoke(this, "Connecting...");
+
+            Host = host;
+            Port = port;
+
+            if (host == "test") {
+                appMain.SwitchToDebugMode();
+                StatusChanged?.Invoke(this, "");
+                return true;
+            }
+
+            if (int.TryParse(port, out int portNumber)) {
+                if (await client.ConnectAsync(host, portNumber)) {
+                    StatusChanged?.Invoke(this, "");
+                    return true;
+                } else {
+                    StatusChanged?.Invoke(this, "Connection failed");
+                }
+            } else {
+                StatusChanged?.Invoke(this, "Invalid port number");
+            }
+
+            return false;
+        }
+    }
+}
