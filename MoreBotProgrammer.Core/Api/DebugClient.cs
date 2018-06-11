9f59b732ccda5d4cec1734c4beef4f278815a126
sazanality <hieu@moretech.co>
2018-06-11 17:58:24 -0500

    Add debug mode

---
+++ MoreBotProgrammer.Core/Api/DebugClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace MoreBotProgrammer.Core
+{
+    public class DebugClient : IBotClient
+    {
+        public event EventHandler Disconnected;
+
+        public async Task<bool> ConnectAsync(string host, int portNumber)
+        {
+            Debug.WriteLine("Connecting to " + host + ":" + portNumber);
+            await Task.Delay(100);
+            return true;
+        }
+
+        public void Disconnect()
+        {
+            Disconnected?.Invoke(this, EventArgs.Empty);
+        }
+
+        public async Task SendMessage(CodeMessage message)
+        {
+            Debug.WriteLine(message.Serialize());
+            await Task.Delay(100);
+        }
+    }
+}
--- MoreBotProgrammer.Core/AppMain.cs
+++ MoreBotProgrammer.Core/AppMain.cs
@@ -15,5 +15,21 @@
         {
             return new ProgrammerViewModel(compiler);
         }
+
+        internal void SwitchToDebugMode()
+        {
+            if (!(client is DebugClient)) {
+                client = new DebugClient();
+                compiler = new Compiler(client);
+            }
+        }
+
+        internal void SwitchToReleaseMode()
+        {
+            if (client is DebugClient) {
+                client = new BotClient();
+                compiler = new Compiler(client);
+            }
+        }
     }
 }
