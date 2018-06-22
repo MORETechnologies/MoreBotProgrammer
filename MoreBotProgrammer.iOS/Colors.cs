ff004fb067275bbbf78d7887be4413b7b8485c39
sazanality <hieu@moretech.co>
2018-06-21 22:06:39 -0500

    Add first draft of color palette

---
+++ MoreBotProgrammer.iOS/Colors.cs
@@ -0,0 +1,13 @@
+﻿using UIKit;
+
+namespace MoreBotProgrammer.iOS
+{
+    public static class Colors
+    {
+        public static readonly UIColor Primary = UIColor.FromHSBA(0, 0, 0.4f, 1);
+        public static readonly UIColor PrimaryLight = UIColor.FromHSBA(0, 0, 0.8f, 1);
+        public static readonly UIColor PrimaryDark = UIColor.FromHSBA(0, 0, 0.2f, 1);
+        public static readonly UIColor DarkText = UIColor.FromHSBA(0, 0, 0.2f, 1);
+        public static readonly UIColor LightText = UIColor.FromHSBA(0, 0, 1, 1);
+    }
+}
