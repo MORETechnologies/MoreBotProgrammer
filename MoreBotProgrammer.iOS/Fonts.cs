a15bbb6a531fdbcb6c7bd67f4c8cb0e71f75165e
sazanality <hieu@moretech.co>
2018-06-26 22:11:55 -0500

    Add new fonts

---
+++ MoreBotProgrammer.iOS/Fonts.cs
@@ -0,0 +1,25 @@
+﻿using UIKit;
+
+namespace MoreBotProgrammer.iOS
+{
+    public static class Fonts
+    {
+        public static readonly UIFont MainFont = UIFont.FromName("OpenSans-Regular", 17);
+        public static readonly UIFont MainFontBold = UIFont.FromName("OpenSans-Bold", 17);
+        public static readonly UIFont MainFontLight = UIFont.FromName("OpenSans-Light", 17);
+
+        public static void ApplyFont(UIFont font, params UILabel[] labels)
+        {
+            foreach (var label in labels) {
+                label.Font = font.WithSize(label.Font.PointSize);
+            }
+        }
+
+        public static void ApplyFont(UIFont font, params UITextField[] textFields)
+        {
+            foreach (var field in textFields) {
+                field.Font = font.WithSize(field.Font.PointSize);
+            }
+        }
+    }
+}
